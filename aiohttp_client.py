85c0d031b6cfefb557db0f8ecdc6dfcc93816035
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-07-19 11:00:44 +0200

    Demonstrate waiting with timeout

--- aiohttp_client.py
+++ aiohttp_client.py
@@ -9,10 +9,23 @@ def go(client):
     text = yield from resp.text()
     print(text)
     yield from resp.release()
+    print("-"*60)
+
+@asyncio.coroutine
+def go_with_timeout(client):
+    print('Get data from httpbin.org with timeout')
+    try:
+        coro = client.get('http://httpbin.org/delay/10')
+        yield from asyncio.wait_for(coro, 2)
+    except asyncio.TimeoutError:
+        print("Timeout after waiting for 2 minutes")
+    else:
+        print("No timeout exception, should never happen")
 
 
 loop = asyncio.get_event_loop()
 client = aiohttp.ClientSession()
 loop.run_until_complete(go(client))
+loop.run_until_complete(go_with_timeout(client))
 client.close()
 print("DONE")
